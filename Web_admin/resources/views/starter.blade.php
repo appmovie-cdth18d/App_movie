76dda2fc9602afb103996827ea4a8929c87cb55f
vothanhtan131 <votai630@gmail.com>
2020-11-23 22:45:21 +0700

    demo web admin

--- Web_admin/resources/views/starter.blade.php
+++ Web_admin/resources/views/starter.blade.php
@@ -9,7 +9,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <meta http-equiv="x-ua-compatible" content="ie=edge">
 
-  <title>AdminLTE 3 | Starter</title>
+  <title>T-Cinima | Starter</title>
 
   <!-- Font Awesome Icons -->
   <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css')}}">
@@ -17,22 +17,26 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css')}}">
   <!-- Google Font: Source Sans Pro -->
   <link href="{{ asset('https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700')}}" rel="stylesheet">
+  <!-- Ionicons -->
+  <link rel="stylesheet" href="{{ asset('https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css') }}">
+  <!-- fullCalendar -->
+  <link rel="stylesheet" href="{{ asset('plugins/fullcalendar/main.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('plugins/fullcalendar-daygrid/main.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('plugins/fullcalendar-timegrid/main.min.css') }}">
+  <link rel="stylesheet" href="{{ asset('plugins/fullcalendar-bootstrap/main.min.css') }}">
 </head>
 <body class="hold-transition sidebar-mini">
 <div class="wrapper">
 
   <!-- Navbar -->
-  <nav class="main-header navbar navbar-expand navbar-white navbar-light">
+  <nav class="main-header navbar navbar-expand navbar-orange navbar-light">
     <!-- Left navbar links -->
     <ul class="navbar-nav">
       <li class="nav-item">
         <a class="nav-link" data-widget="pushmenu" href="#" role="button"><i class="fas fa-bars"></i></a>
       </li>
       <li class="nav-item d-none d-sm-inline-block">
-        <a href="index3.html" class="nav-link">Home</a>
-      </li>
-      <li class="nav-item d-none d-sm-inline-block">
-        <a href="#" class="nav-link">Contact</a>
+        <a href="{{ asset('layouts/index') }}" class="nav-link">Home</a>
       </li>
     </ul>
 
@@ -146,10 +150,10 @@ scratch. This page gets rid of all links and provides the needed markup only.
   <!-- Main Sidebar Container -->
   <aside class="main-sidebar sidebar-dark-primary elevation-4">
     <!-- Brand Logo -->
-    <a href="index3.html" class="brand-link">
+    <a href="{{ asset('layouts/index') }}" class="brand-link">
       <img src="{{ asset('dist/img/AdminLTELogo.png')}}" alt="AdminLTE Logo" class="brand-image img-circle elevation-3"
            style="opacity: .8">
-      <span class="brand-text font-weight-light">AdminLTE 3</span>
+      <span class="brand-text font-weight-light">T-Cinima</span>
     </a>
 
     <!-- Sidebar -->
@@ -160,7 +164,7 @@ scratch. This page gets rid of all links and provides the needed markup only.
           <img src="{{ asset('dist/img/user2-160x160.jpg')}}" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block">Alexander Pierce</a>
+          <a href="#" class="d-block">Võ Thanh Tấn</a>
         </div>
       </div>
 
@@ -169,38 +173,173 @@ scratch. This page gets rid of all links and provides the needed markup only.
         <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
           <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-          <li class="nav-item has-treeview menu-open">
-            <a href="#" class="nav-link active">
+          <li class="nav-item has-treeview">
+            <a href="{{ asset('layouts/index') }}" class="nav-link active">
               <i class="nav-icon fas fa-tachometer-alt"></i>
               <p>
-                Starter Pages
-                <i class="right fas fa-angle-left"></i>
+                Trang Tổng Quan
+              </p>
+            </a>
+          </li>
+          
+          <!--tables-thong-tin-khach-hang-->
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-table"></i>
+              <p>
+                Thông Tin Khách Hàng
+                <i class="fas fa-angle-left right"></i>
               </p>
             </a>
             <ul class="nav nav-treeview">
               <li class="nav-item">
-                <a href="#" class="nav-link active">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Active Page</p>
+                <a href=" {{ asset('data/data_khachhang')}}" class="nav-link">
+                  
+                  <p>Dữ Liệu Bảng</p>
                 </a>
               </li>
               <li class="nav-item">
-                <a href="#" class="nav-link">
-                  <i class="far fa-circle nav-icon"></i>
-                  <p>Inactive Page</p>
+                <a href="{{ asset('data/them_khachhang') }}" class="nav-link">
+                  
+                  <p>Thêm</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ asset('data/sua_khachhang') }}" class="nav-link">
+                  
+                  <p>Sửa</p>
                 </a>
               </li>
             </ul>
           </li>
-          <li class="nav-item">
+          <!--endtables-->
+          <!--tables-thong-tin-phim-->
+          <li class="nav-item has-treeview">
             <a href="#" class="nav-link">
-              <i class="nav-icon fas fa-th"></i>
+              <i class="nav-icon fas fa-table"></i>
               <p>
-                Simple Link
-                <span class="right badge badge-danger">New</span>
+                Phim
+                <i class="fas fa-angle-left right"></i>
               </p>
             </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href=" {{ asset('data/data_phim')}}" class="nav-link">
+                  
+                  <p>Dữ Liệu Bảng</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ asset('data/them_phim') }}" class="nav-link">
+                  
+                  <p>Thêm</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ asset('data/sua_phim') }}" class="nav-link">
+                  
+                  <p>Sửa</p>
+                </a>
+              </li>
+
+            </ul>
           </li>
+          <!--endtables-->
+          <!--tables-thong-tin-rap-->
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-table"></i>
+              <p>
+                Rap
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href=" {{ asset('data/data_rap')}}" class="nav-link">
+                  
+                  <p>Dữ Liệu Bảng</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ asset('data/them_rap') }}" class="nav-link">
+                  
+                  <p>Thêm</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ asset('data/sua_rap') }}" class="nav-link">
+                  
+                  <p>Sửa</p>
+                </a>
+              </li>
+
+            </ul>
+          </li>
+          <!--endtables-->
+          <!--tables-thong-tin-lich-chiueu-->
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-table"></i>
+              <p>
+                Lịch Chiếu
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href=" {{ asset('data/data_lichchieu')}}" class="nav-link">
+                  
+                  <p>Dữ Liệu Bảng</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ asset('data/them_lichchieu') }}" class="nav-link">
+                  
+                  <p>Thêm</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ asset('data/sua_lichchieu') }}" class="nav-link">
+                  <p>Sửa</p>
+                </a>
+              </li>
+
+            </ul>
+          </li>
+          <!--endtables-->
+          <!--tables-thong-tin-khach-hang-->
+          <li class="nav-item has-treeview">
+            <a href="#" class="nav-link">
+              <i class="nav-icon fas fa-table"></i>
+              <p>
+                Tài Khoản
+                <i class="fas fa-angle-left right"></i>
+              </p>
+            </a>
+            <ul class="nav nav-treeview">
+              <li class="nav-item">
+                <a href=" {{ asset('data/data_taikhoan')}}" class="nav-link">
+                  
+                  <p>Dữ Liệu Bảng</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ asset('data/them_taikhoan') }}" class="nav-link">
+                  
+                  <p>Thêm</p>
+                </a>
+              </li>
+              <li class="nav-item">
+                <a href="{{ asset('data/sua_taikhoan') }}" class="nav-link">
+                  
+                  <p>Sửa</p>
+                </a>
+              </li>
+
+            </ul>
+          </li>
+          <!--endtables-->
         </ul>
       </nav>
       <!-- /.sidebar-menu -->
@@ -208,34 +347,14 @@ scratch. This page gets rid of all links and provides the needed markup only.
     <!-- /.sidebar -->
   </aside>
 
-  <!-- Content Wrapper. Contains page content -->
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-      <div class="container-fluid">
-        <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1 class="m-0 text-dark">Starter Page</h1>
-          </div><!-- /.col -->
-          <div class="col-sm-6">
-            <ol class="breadcrumb float-sm-right">
-              <li class="breadcrumb-item"><a href="#">Home</a></li>
-              <li class="breadcrumb-item active">Starter Page</li>
-            </ol>
-          </div><!-- /.col -->
-        </div><!-- /.row -->
-      </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
-    <!--/main-content-->>
+    @yield('content')
 
   <!-- Control Sidebar -->
   <aside class="control-sidebar control-sidebar-dark">
     <!-- Control sidebar content goes here -->
     <div class="p-3">
-      <h5>Title</h5>
-      <p>Sidebar content</p>
+      <h5>Cài Đặt</h5>
+      <p>Đăng Xuất</p>
     </div>
   </aside>
   <!-- /.control-sidebar -->
@@ -260,5 +379,187 @@ scratch. This page gets rid of all links and provides the needed markup only.
 <script src="{{ asset('plugins/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
 <!-- AdminLTE App -->
 <script src="{{ asset('dist/js/adminlte.min.js')}}"></script>
+<script src="{{ asset('plugins/jquery-ui/jquery-ui.min.js')}}"></script>
+
+
+<!-- AdminLTE for demo purposes -->
+<script src="{{ asset('dist/js/demo.js')}}"></script>
+<!-- fullCalendar 2.2.5 -->
+<script src="{{ asset('plugins/moment/moment.min.js')}}"></script>
+<script src="{{ asset('plugins/fullcalendar/main.min.js')}}"></script>
+<script src="{{ asset('plugins/fullcalendar-daygrid/main.min.js')}}"></script>
+<script src="{{ asset('plugins/fullcalendar-timegrid/main.min.js')}}"></script>
+<script src="{{ asset('plugins/fullcalendar-interaction/main.min.js')}}"></script>
+<script src="{{ asset('plugins/fullcalendar-bootstrap/main.min.js')}}"></script>
+<script>
+  $(function () {
+
+    /* initialize the external events
+     -----------------------------------------------------------------*/
+    function ini_events(ele) {
+      ele.each(function () {
+
+        // create an Event Object (http://arshaw.com/fullcalendar/docs/event_data/Event_Object/)
+        // it doesn't need to have a start or end
+        var eventObject = {
+          title: $.trim($(this).text()) // use the element's text as the event title
+        }
+
+        // store the Event Object in the DOM element so we can get to it later
+        $(this).data('eventObject', eventObject)
+
+        // make the event draggable using jQuery UI
+        $(this).draggable({
+          zIndex        : 1070,
+          revert        : true, // will cause the event to go back to its
+          revertDuration: 0  //  original position after the drag
+        })
+
+      })
+    }
+
+    ini_events($('#external-events div.external-event'))
+
+    /* initialize the calendar
+     -----------------------------------------------------------------*/
+    //Date for the calendar events (dummy data)
+    var date = new Date()
+    var d    = date.getDate(),
+        m    = date.getMonth(),
+        y    = date.getFullYear()
+
+    var Calendar = FullCalendar.Calendar;
+    var Draggable = FullCalendarInteraction.Draggable;
+
+    var containerEl = document.getElementById('external-events');
+    var checkbox = document.getElementById('drop-remove');
+    var calendarEl = document.getElementById('calendar');
+
+    // initialize the external events
+    // -----------------------------------------------------------------
+
+    new Draggable(containerEl, {
+      itemSelector: '.external-event',
+      eventData: function(eventEl) {
+        console.log(eventEl);
+        return {
+          title: eventEl.innerText,
+          backgroundColor: window.getComputedStyle( eventEl ,null).getPropertyValue('background-color'),
+          borderColor: window.getComputedStyle( eventEl ,null).getPropertyValue('background-color'),
+          textColor: window.getComputedStyle( eventEl ,null).getPropertyValue('color'),
+        };
+      }
+    });
+
+    var calendar = new Calendar(calendarEl, {
+      plugins: [ 'bootstrap', 'interaction', 'dayGrid', 'timeGrid' ],
+      header    : {
+        left  : 'prev,next today',
+        center: 'title',
+        right : 'dayGridMonth,timeGridWeek,timeGridDay'
+      },
+      'themeSystem': 'bootstrap',
+      //Random default events
+      events    : [
+        {
+          title          : 'All Day Event',
+          start          : new Date(y, m, 1),
+          backgroundColor: '#f56954', //red
+          borderColor    : '#f56954', //red
+          allDay         : true
+        },
+        {
+          title          : 'Long Event',
+          start          : new Date(y, m, d - 5),
+          end            : new Date(y, m, d - 2),
+          backgroundColor: '#f39c12', //yellow
+          borderColor    : '#f39c12' //yellow
+        },
+        {
+          title          : 'Meeting',
+          start          : new Date(y, m, d, 10, 30),
+          allDay         : false,
+          backgroundColor: '#0073b7', //Blue
+          borderColor    : '#0073b7' //Blue
+        },
+        {
+          title          : 'Lunch',
+          start          : new Date(y, m, d, 12, 0),
+          end            : new Date(y, m, d, 14, 0),
+          allDay         : false,
+          backgroundColor: '#00c0ef', //Info (aqua)
+          borderColor    : '#00c0ef' //Info (aqua)
+        },
+        {
+          title          : 'Birthday Party',
+          start          : new Date(y, m, d + 1, 19, 0),
+          end            : new Date(y, m, d + 1, 22, 30),
+          allDay         : false,
+          backgroundColor: '#00a65a', //Success (green)
+          borderColor    : '#00a65a' //Success (green)
+        },
+        {
+          title          : 'Click for Google',
+          start          : new Date(y, m, 28),
+          end            : new Date(y, m, 29),
+          url            : 'http://google.com/',
+          backgroundColor: '#3c8dbc', //Primary (light-blue)
+          borderColor    : '#3c8dbc' //Primary (light-blue)
+        }
+      ],
+      editable  : true,
+      droppable : true, // this allows things to be dropped onto the calendar !!!
+      drop      : function(info) {
+        // is the "remove after drop" checkbox checked?
+        if (checkbox.checked) {
+          // if so, remove the element from the "Draggable Events" list
+          info.draggedEl.parentNode.removeChild(info.draggedEl);
+        }
+      }    
+    });
+
+    calendar.render();
+    // $('#calendar').fullCalendar()
+
+    /* ADDING EVENTS */
+    var currColor = '#3c8dbc' //Red by default
+    //Color chooser button
+    var colorChooser = $('#color-chooser-btn')
+    $('#color-chooser > li > a').click(function (e) {
+      e.preventDefault()
+      //Save color
+      currColor = $(this).css('color')
+      //Add color effect to button
+      $('#add-new-event').css({
+        'background-color': currColor,
+        'border-color'    : currColor
+      })
+    })
+    $('#add-new-event').click(function (e) {
+      e.preventDefault()
+      //Get value and make sure it is not null
+      var val = $('#new-event').val()
+      if (val.length == 0) {
+        return
+      }
+
+      //Create events
+      var event = $('<div />')
+      event.css({
+        'background-color': currColor,
+        'border-color'    : currColor,
+        'color'           : '#fff'
+      }).addClass('external-event')
+      event.html(val)
+      $('#external-events').prepend(event)
+
+      //Add draggable funtionality
+      ini_events(event)
+
+      //Remove event from text input
+      $('#new-event').val('')
+    })
+  })
+</script>
 </body>
 </html>
--- Web_admin/routes/web.php
+++ Web_admin/routes/web.php
@@ -12,10 +12,57 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-
 Route::get('/', function () {
-    return view('welcome');
+    return view('layouts.login');
 });
-Route::get('/starter', function () {
-    return view('starter');
+Route::get('/register', function () {
+    return view('layouts.register');
+});
+Route::get('/layouts/index', function () {
+    return view('layouts.index');
+});
+Route::get('/data/data_khachhang', function () {
+    return view('data.data_khachhang');
+});
+Route::get('/data/data_rap', function () {
+    return view('data.data_rap');
+});
+Route::get('/data/data_phim', function () {
+    return view('data.data_phim');
+});
+Route::get('/data/data_lichchieu', function () {
+    return view('data.data_lichchieu');
+});
+Route::get('/data/data_taikhoan', function () {
+    return view('data.data_taikhoan');
+});
+Route::get('/data/them_khachhang', function () {
+    return view('data.them_khachhang');
+});
+Route::get('/data/them_lichchieu', function () {
+    return view('data.them_lichchieu');
+});
+Route::get('/data/them_rap', function () {
+    return view('data.them_rap');
+});
+Route::get('/data/them_taikhoan', function () {
+    return view('data.them_taikhoan');
+});
+Route::get('/data/them_phim', function () {
+    return view('data.them_phim');
+});
+Route::get('/data/sua_khachhang', function () {
+    return view('data.sua_khachhang');
+});
+Route::get('/data/sua_lichchieu', function () {
+    return view('data.sua_lichchieu');
+});
+Route::get('/data/sua_rap', function () {
+    return view('data.sua_rap');
+});
+Route::get('/data/sua_taiKhoan', function () {
+    return view('data.sua_taiKhoan');
+});
+Route::get('/data/sua_phim', function () {
+    return view('data.sua_phim');
 });
